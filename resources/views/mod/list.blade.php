d21bd1a26f28c0561d842632b23ab4067bcdb333
Rustypredator <mail@rusty.info>
2021-07-07 18:57:37 +0200

    js for upload

--- resources/views/mod/list.blade.php
+++ resources/views/mod/list.blade.php
@@ -74,7 +74,14 @@ $(document).ready(function() {
 	$('#dataTables').dataTable({
 		"order": [[ 1, "asc" ]]
 	});
-	$('#fileupload').fileupload();
+	$('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p></p>').text(file.name).appendTo(document.body);
+            });
+        }
+    });
 });
 </script>
 @endsection
