76d598a0511fabeeb49fc3f8909719bb2406c36e
Rustypredator <mail@rusty.info>
2021-07-08 11:10:14 +0200

    add noscript stuff, move js to bottom

--- resources/views/mod/list.blade.php
+++ resources/views/mod/list.blade.php
@@ -7,6 +7,10 @@
 @section('top')
 <link href="{{ asset('assets/jQueryFileUpload/css/jquery.fileupload.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/jQueryFileUpload/css/jquery.fileupload-ui.css') }}" rel="stylesheet">
+<noscript>
+	<link href="{{ asset('assets/jQueryFileUpload/css/jquery.fileupload-noscript.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/jQueryFileUpload/css/jquery.fileupload-ui-noscript.css') }}" rel="stylesheet">
+</noscript>
 @stop
 
 @section('content')
@@ -19,7 +23,44 @@
 		Upload Mods
 	</div>
 	<div class="panel-body">
-		<form id="fileupload" action="{{URL::to('mod/upload')}}" method="POST" enctype="multipart/form-data"></form>
+		<form id="fileupload" action="/mod/upload" method="POST" enctype="multipart/form-data" >
+			<div class="row fileupload-buttonbar">
+				<div class="col-lg-7">
+					<span class="btn btn-success fileinput-button">
+						<i class="glyphicon glyphicon-plus"></i>
+						<span>Add files...</span>
+						<input type="file" name="files[]" multiple />
+					</span>
+					<button type="submit" class="btn btn-primary start">
+						<i class="glyphicon glyphicon-upload"></i>
+						<span>Start upload</span>
+					</button>
+					<button type="reset" class="btn btn-warning cancel">
+						<i class="glyphicon glyphicon-ban-circle"></i>
+						<span>Cancel upload</span>
+					</button>
+					<button type="button" class="btn btn-danger delete">
+						<i class="glyphicon glyphicon-trash"></i>
+						<span>Delete selected</span>
+					</button>
+					<input type="checkbox" class="toggle" />
+					<span class="fileupload-process"></span>
+				</div>
+				<!-- The global progress state -->
+				<div class="col-lg-5 fileupload-progress fade">
+					<!-- The global progress bar -->
+					<div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" >
+						<div class="progress-bar progress-bar-success" style="width: 0%;" ></div>
+					</div>
+					<!-- The extended global progress state -->
+					<div class="progress-extended">&nbsp;</div>
+				</div>
+			</div>
+			<!-- The table listing the files available for upload/download -->
+			<table role="presentation" class="table table-striped">
+				<tbody class="files"></tbody>
+			</table>
+		</form>
 	</div>
 </div>
 
@@ -79,13 +120,13 @@
 @section('bottom')
 <script src="{{ asset('assets/jQueryFileUpload/js/vendor/jquery.ui.widget.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/jquery.iframe-transport.js') }}"></script>
-<script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload-process.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload-image.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload-audio.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload-video.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload-validate.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload-ui.js') }}"></script>
+<script src="{{ asset('assets/jQueryFileUpload/js/jquery.fileupload.js') }}"></script>
 <script src="{{ asset('assets/jQueryFileUpload/js/init.js') }}"></script>
 <script type="text/javascript">
 $(document).ready(function() {
