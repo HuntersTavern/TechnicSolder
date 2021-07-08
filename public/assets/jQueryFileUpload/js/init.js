5a7b46f8e374dfa9382cd88587117456ce1ff7d9
Rustypredator <mail@rusty.info>
2021-07-08 08:26:13 +0200

    add init script for upload

---
+++ public/assets/jQueryFileUpload/js/init.js
@@ -0,0 +1,27 @@
+$(function () {
+    'use strict';
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload({
+        url: '/mod/upload'
+    });
+    // Load existing files:
+    $('#fileupload').addClass('fileupload-processing');
+    $.ajax({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        context: $('#fileupload')[0]
+    })
+    .always(function () {
+        $(this).removeClass('fileupload-processing');
+    })
+    .done(function (result) {
+        $(this)
+            .fileupload('option', 'done')
+            // eslint-disable-next-line new-cap
+            .call(this, $.Event('done'), {
+                result: result
+            });
+    });
+});
