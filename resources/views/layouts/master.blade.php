4f997bdf11be4306e8213a8cef695e197502673f
Rustypredator <mail@rusty.info>
2021-07-08 13:51:45 +0200

    adjust assets accordingly

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,10 +7,10 @@
     @show
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/Bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/jQuery/css/jquery.jgrowl.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/sb-admin.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/jquery.jgrowl.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/datatables.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/metisMenu.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/OpenSansfont.css') }}" rel="stylesheet">
@@ -123,14 +123,14 @@
     <!-- /.row -->
 </div>
 <!-- /#page-wrapper -->
-<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-<script src="{{ asset('js/jquery-migrate-3.0.0.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/jquery.jgrowl.min.js') }}"></script>
+<script src="{{ asset('assets/jQuery/js/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('assets/jQuery/js/jquery-migrate-3.0.0.min.js') }}"></script>
+<script src="{{ asset('assets/jQuery/js/jquery.jgrowl.min.js') }}"></script>
+<script src="{{ asset('assets/jQuery/js/jquery.slugify.js') }}"></script>
+<script src="{{ asset('assets/Bootstrap/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('js/metisMenu.min.js') }}"></script>
 <script src="{{ asset('js/sb-admin.js') }}"></script>
 <script src="{{ asset('js/datatables.min.js') }}"></script>
-<script src="{{ asset('js/jquery.slugify.js') }}"></script>
 <script src="{{ asset('js/nav-float.js') }}"></script>
 <!--Custom Scripts-->
 <script type="text/javascript">
